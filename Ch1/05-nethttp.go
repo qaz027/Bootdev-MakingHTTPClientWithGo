3f3f397fcb74330acd8d73b39307a9a4ec133d26
QuimbanoPrime <qaz027@pm.me>
2026-09-14 23:43:35 -0700

    ch1 exercise 5 - net/http

---
+++ Ch1/05-nethttp.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func getIssueData(url string) ([]byte, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+	defer res.Body.Close()
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response: %w", err)
+	}
+	return data, nil
+	//return res, nil
+}
