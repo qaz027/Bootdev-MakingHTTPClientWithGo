ce62619a44cb98cb79a6b037d1503e90d8688e89
QuimbanoPrime <qaz027@pm.me>
2026-09-18 20:29:33 -0700

    through lesson 4 in json

---
+++ Ch2/04-unmarshallingJSON.go
@@ -0,0 +1,31 @@
+// package ch2
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func getIssues(url string) ([]Issue, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+	defer res.Body.Close()
+
+	// ?
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var issues []Issue
+	if err := json.Unmarshal(data, &issues); err != nil {
+		return nil, err
+	} else {
+		return issues, nil
+	}
+
+}
---
+++ Ch2/issue.go
@@ -0,0 +1,7 @@
+// package ch2
+package main
+
+type Issue struct {
+	Title    string `json:"title"`
+	Estimate int    `json:"estimate"`
+}
