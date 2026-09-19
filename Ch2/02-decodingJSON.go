57fe3f3c7afa113518bbf85388caa4a7f0df75b3
QuimbanoPrime <qaz027@pm.me>
2026-09-18 20:20:10 -0700

    decoding json exercise

---
+++ Ch2/02-decodingJSON.go
@@ -0,0 +1,25 @@
+// package ch2
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func getIssues(url string) ([]Issue, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+	defer res.Body.Close()
+
+	// ?
+	var issues []Issue
+	decoder := json.NewDecoder(res.Body)
+	if err := decoder.Decode(&issues); err != nil {
+		return nil, fmt.Errorf("error decoding response body: %w", err)
+
+	}
+	return issues, nil
+}
