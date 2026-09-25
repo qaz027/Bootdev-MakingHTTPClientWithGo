215f2c20c48bd322dcddf212e890e59f525230fb
QuimbanoPrime <qaz027@pm.me>
2026-09-24 20:39:48 -0700

    reorganizing folder

---
+++ Ch1/06-webservers.go
@@ -0,0 +1,22 @@
+//package makehttpclientsgo
+/*
+# Web Servers
+
+Up to this point, most of the data you have worked with in your code has simply been generated and stored locally in variables.
+
+While you'll always use variables to store and manipulate data while your program is running, most websites and apps use a web server to store, sort, and serve that data so that it sticks around for longer than a single session, and can be accessed by multiple devices.
+
+## Listening and Serving Data
+
+Similar to how a server at a restaurant brings your food to the table, a [web server](https://en.wikipedia.org/wiki/Web_server) serves web resources, such as web pages, images, and other data. The server is turned on and "listening" for inbound requests constantly so that the second it receives a new request, it can send an appropriate response.
+
+## The Server Is the Back-End
+
+While the "front-end" of a website or web application is the device the user interacts with, the "back-end" is the server that keeps all the data housed in a central location.
+
+## A Server Is Just a Computer
+
+"Server" is just the name we give to a computer that is taking on the role of serving data across a network connection. A good server is turned on and available 24 hours a day, 7 days a week. While your laptop _can_ be used as a server, it makes more sense to use a computer in a data center that's designed to be up and running constantly.
+*/
+
+// nothing here.
